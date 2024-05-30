cb0d4fbefe3c77e67556eee510ad6347a66599a3
SnehaKoirala <345788@via.dk>
2024-05-30 13:32:42 +0200

    Update DatabaseConnection.java

--- src/server/database/DatabaseConnection.java
+++ src/server/database/DatabaseConnection.java
@@ -5,33 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection
+          
+            
 
-    private static DatabaseConnection instance;
-    private Connection connection;
+                ("jdbc:postgresql://localhost:5432/postgres?currentSchema=sep2_rms",
+                        "postgres", "P@ssw0rd");
 
-    // Private constructor to prevent instantiation
-    private DatabaseConnection() throws SQLException {
-        try {
-            connection = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/postgres?currentSchema=sep2_rms",
-
-                    "postgres", "P@ssw0rd");
-
-        } catch (SQLException e) {
-            throw new SQLException("Failed to create the database connection.", e);
-        }
-    }
-
-    // Method to get the singleton instance of DatabaseConnection
-    public static DatabaseConnection getInstance() throws SQLException {
-        if (instance == null || instance.getConnection().isClosed()) {
-            instance = new DatabaseConnection();
-        }
-        return instance;
-    }
-
-    // Method to get the database connection
-    public Connection getConnection() {
-        return connection;
     }
 }
